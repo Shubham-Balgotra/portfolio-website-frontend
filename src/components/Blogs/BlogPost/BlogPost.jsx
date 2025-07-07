574c8f7f4b99941270f315a5c74a1b91506e77f9
Shubham Balgotra <52531006+Shubham-Balgotra@users.noreply.github.com>
2025-07-07 16:55:54 +0530

    Update BlogPost.jsx

--- src/components/Blogs/BlogPost/BlogPost.jsx
+++ src/components/Blogs/BlogPost/BlogPost.jsx
@@ -23,7 +23,7 @@ const BlogPost = ({ darkMode }) => {
   if (!blog) {
     return (
       <div
-        className={`flex items-center justify-center`}
+        className={` min-h-screen flex items-center justify-center`}
       >
         <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       </div>
