3603058f3871c91ba731f50c923afeaf80542bf2
Shubham Balgotra <52531006+Shubham-Balgotra@users.noreply.github.com>
2025-07-08 19:27:17 +0530

    Update BlogPost.jsx

--- src/components/Blogs/BlogPost/BlogPost.jsx
+++ src/components/Blogs/BlogPost/BlogPost.jsx
@@ -33,54 +33,9 @@ const BlogPost = ({ darkMode }) => {
   }
 
   return (
-    // <div
-    //   className={` mt-3 max-w-screen-2xl mx-auto px-10 py-16 `}
-    // >
-    //   {/* Date */}
-    //   <p className={`text-sm ${darkMode ? "text-gray-400" : "text-gray-700"} mb-2`}>
-    //     🗓️ Written on:&nbsp;
-    //     {new Date(blog.date).toLocaleDateString(undefined, {
-    //       year: "numeric",
-    //       month: "long",
-    //       day: "numeric",
-    //     })}
-    //   </p>
-
-    //   {/* Title */}
-    //   <h1
-    //     className={`text-4xl font-bold ${darkMode ? "text-white" : "text-gray-900"} mb-6 leading-snug`}
-    //   >
-    //     {blog.title}
-    //   </h1>
-
-    //   {/* Tags */}
-    //   <div className="flex flex-wrap gap-2 mb-8">
-    //     {blog.tags.map((tag, i) => (
-    //       <span
-    //         key={i}
-    //         className={`${darkMode?" bg-gray-900 text-gray-300":" bg-blue-50 text-blue-700"}bg-blue-50 text-blue-700 px-3 py-1 rounded-full text-sm font-medium`}
-    //       >
-    //         #{tag}
-    //       </span>
-    //     ))}
-    //   </div>
-
-    //   {/* Markdown Content */}
-    //   <div
-    //     className={`prose prose-lg max-w-none leading-relaxed ${
-    //       darkMode ? "text-gray-300" : "text-gray-900"
-    //     }`}
-    //     style={{
-    //       fontFamily: "'Merriweather', serif",
-    //       fontSize: "1.125rem",
-    //       // textAlign: "justify",
-    //     }}
-    //   >
-    //     <ReactMarkdown>{blog.content}</ReactMarkdown>
-    //   </div>
-    // </div>
+   
        <div
-      className={`min-h-screen  mt-2 max-w-screen-2xl mx-auto  sm:px-3 md:px-10 py-8 sm:py-12 md:py-16`}
+      className={`min-h-screen  mt-2 max-w-screen-2xl mx-auto  sm:px-3 md:px-10 py-8 sm:py-12 md:py-10`}
     >
       {/* Date */}
       <p
