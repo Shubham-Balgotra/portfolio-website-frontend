891861d3a9680803f956dbc3b465894d51948ff8
Shubham Balgotra <52531006+Shubham-Balgotra@users.noreply.github.com>
2025-07-10 00:27:42 +0530

    Update ClientProcess.jsx

--- src/components/Home/ClientProcess.jsx
+++ src/components/Home/ClientProcess.jsx
@@ -27,38 +27,42 @@ const steps = [
 export default function ClientProcess({ darkMode }) {
   return (
     <section
-      className={`w-full  px-4 py-10 lg:py-14 mx-auto ${ //md:px-12 lg:px-24 xl:px-48 
-        darkMode ? "bg-gray-1000 text-gray-300" : "bg-gray-50 text-gray-800"
+      className={`relative w-full px-4 py-10 lg:py-14 flex items-center justify-center min-h-screen ${
+        darkMode ? "bg-gray-950 text-gray-300" : "bg-gray-50 text-gray-800"
       }`}
     >
-      <h2 className="text-2xl md:text-3xl font-bold mb-6 border-b pb-2">
-         My Work Process
-      </h2>
+      <div className="max-w-6xl w-full flex flex-col items-center justify-center">
+        <div className={`w-full border-b ${darkMode?"border-gray-300":"border-gray-900"}   mb-6 pb-2`}>
+          <h1 className={`text-2xl md:text-3xl lg:text-4xl  font-bold ${darkMode?"text-gray-300":"text-gray-900"}  text-center mb-2`}>
+            My Work Process
+          </h1>
+        </div>
 
-      <div className="grid gap-12  sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 lg:px-4">
-        {steps.map((step, index) => (
-          <div
-            key={index}
-            className={`rounded-xl p-6 border transition duration-300 shadow-md hover:shadow-xl ${
-              darkMode
-                ? "bg-gray-950 text-gray-300 border-gray-700 shadow-gray-800"
-                : "bg-white text-gray-700 border-gray-200 shadow-gray-100"
-            }`}
-          >
-            <div className="flex items-center gap-3 mb-2">
-              <CheckCircle
-                className={`${
-                  darkMode ? "text-green-400" : "text-green-600"
-                }`}
-                size={22}
-              />
-              <h3 className="text-lg font-semibold">
-                {index + 1}. {step.title}
-              </h3>
+        <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 px-2 sm:px-0">
+          {steps.map((step, index) => (
+            <div
+              key={index}
+              className={`rounded-xl p-6 border transition duration-300 shadow-md hover:shadow-xl ${
+                darkMode
+                  ? "bg-gray-900 text-gray-300 border-gray-700 shadow-gray-800"
+                  : "bg-white text-gray-700 border-gray-200 shadow-gray-100"
+              }`}
+            >
+              <div className="flex items-center gap-3 mb-3">
+                <CheckCircle
+                  className={`${
+                    darkMode ? "text-green-400" : "text-green-600"
+                  }`}
+                  size={22}
+                />
+                <h3 className="text-lg font-semibold">
+                  {index + 1}. {step.title}
+                </h3>
+              </div>
+              <p className="text-sm">{step.desc}</p>
             </div>
-            <p className="text-sm">{step.desc}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
